d01564e2e0009e6d02672edf589c7f12b12623d7
Guillermo Briceno <guillermofbriceno@gmail.com>
2019-04-25 21:29:32 -0400

    Fixed indentation

--- vhdl/cpu_ss.vhdl
+++ vhdl/cpu_ss.vhdl
@@ -6,27 +6,27 @@ entity cpu is
 	port(
 	out_mem_data:	out	std_logic_vector(31 downto 0);
 	in_mem_data:	in	std_logic_vector(31 downto 0);
-	mem_addr:			out	std_logic_vector(31 downto 0);
+	mem_addr:	out	std_logic_vector(31 downto 0);
 	instruction:	in	std_logic_vector(31 downto 0);
-	pc:						out	std_logic_vector(9  downto 0);
-	write_mem: 		out  std_ulogic;
-	clk: 					in  std_ulogic
-	next_instruction:	in	std_logic_vector(31 downto 0); --this is not a hardware implementation, only for the SET inst.
+	pc:		out	std_logic_vector(9  downto 0);
+	write_mem: 	out  	std_ulogic;
+	clk: 		in  	std_ulogic;
+	next_inst:	in	std_logic_vector(31 downto 0); --this is not a hardware implementation, only for the SET inst.
 );
 end cpu;
 
 architecture behave of cpu is
 	--control unit control signals
-	signal halt			: std_logic;
+	signal halt	: std_logic;
 	signal reg_write: std_logic;
-	signal alu_op		: std_logic_vector(3 downto 0);
+	signal alu_op	: std_logic_vector(3 downto 0);
 	signal mem_store: std_logic;
-	signal imm			: std_logic;
-	signal bn				: std_logic;
-	signal bz				: std_logic;
-	signal b				: std_logic;
-	signal load			: std_logic;
-	--signal opcode		: std_logic_vector(4 downto 0); --input opcode
+	signal imm	: std_logic;
+	signal bn	: std_logic;
+	signal bz	: std_logic;
+	signal b	: std_logic;
+	signal load	: std_logic;
+	--signal opcode	: std_logic_vector(4 downto 0); --input opcode
 
 	--program counter datapaths
 	signal pc_addr:	std_logic_vector(9 downto 0); --pc address bus
@@ -35,12 +35,12 @@ architecture behave of cpu is
 
 	component ALU
 	port(
-			A:	in	std_logic_vector(31 downto 0);
-			B:	in	std_logic_vector(31 downto 0);
-			func:	in	std_logic_vector(3 downto 0);
-			outp:	out	std_logic_vector(31 downto 0);
-			neg:	out	std_ulogic;
-			zero: out std_ulogic
+		A:	in	std_logic_vector(31 downto 0);
+		B:	in	std_logic_vector(31 downto 0);
+		func:	in	std_logic_vector(3 downto 0);
+		outp:	out	std_logic_vector(31 downto 0);
+		neg:	out	std_ulogic;
+		zero: out std_ulogic
 	);
 	end component ALU;
 
@@ -57,115 +57,115 @@ begin
 	begin
 		variable opcode := instruction(31 downto 26);
 		if opcode = "00000" then --nop
-			halt 			<= 0;
-			reg_write <= 0;
-			mem_store <= 0;
+			halt		<= 0;
+			reg_write 	<= 0;
+			mem_store 	<= 0;
 			alu_op 		<= "0000";
-			imm 			<= 0;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 0;
+			bz 		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "00001" then --set
-			halt 			<= 0;
-			reg_write <= 1;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 1;
+			mem_store 	<= 0;
 			alu_op 		<= "1111";
-			imm 			<= 1;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 1;
+			bn 		<= 0;
+			bz 		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "00010" then --load
-			halt 			<= 0;
-			reg_write <= 1;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 1;
+			mem_store 	<= 0;
 			alu_op 		<= "0000";
-			imm 			<= 1;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 1;
+			bn 		<= 0;
+			bz 		<= 0;
+			1b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "00011" then --store
-			halt 			<= 0;
-			reg_write <= 0;
-			mem_store <= 1;
+			halt 		<= 0;
+			reg_write 	<= 0;
+			mem_store 	<= 1;
 			alu_op 		<= "1110";
-			imm 			<= 0;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 0;
+			bz 		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "00100" then --move
-			halt 			<= 0;
-			reg_write <= 1;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 1;
+			mem_store 	<= 0;
 			alu_op 		<= "1110";
-			imm 			<= 0;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 0;
+			bz 		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		elsif (unsigned(opcode) >=  5) and (unsigned(opcode) <= 17) then --math operations except POW which is opcode 18
-			halt 			<= 0;
-			reg_write <= 1;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 1;
+			mem_store 	<= 0;
 			alu_op 		<= std_logic_vector( unsigned( opcode(3 downto 0) - 5 ) );
-			imm 			<= 0;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 0;
+			bz		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "10010" then --POW
-			halt 			<= 0;
-			reg_write <= 1;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 1;
+			mem_store 	<= 0;
 			alu_op 		<= "1101";
-			imm 			<= 1;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 1;
+			bn 		<= 0;
+			bz 		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "10011" then --Unconditional branch
-			halt 			<= 0;
-			reg_write <= 0;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 0;
+			mem_store 	<= 0;
 			alu_op 		<= "0000";
-			imm 			<= 0;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 1;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 0;
+			bz 		<= 0;
+			b 		<= 1;
+			load 		<= 0;
 		elsif opcode = "10100" then --BZ
-			halt 			<= 0;
-			reg_write <= 0;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 0;
+			mem_store 	<= 0;
 			alu_op 		<= "1110";
-			imm 			<= 0;
-			bn 				<= 0;
-			bz 				<= 1;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 0;
+			bz 		<= 1;
+			b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "10101" then --BN
-			halt 			<= 0;
-			reg_write <= 0;
-			mem_store <= 0;
+			halt 		<= 0;
+			reg_write 	<= 0;
+			mem_store 	<= 0;
 			alu_op 		<= "1110";
-			imm 			<= 0;
-			bn 				<= 1;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 1;
+			bz 		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		elsif opcode = "10110" then --Halt
-			halt 			<= 1;
-			reg_write <= 0;
-			mem_store <= 0;
+			halt 		<= 1;
+			reg_write 	<= 0;
+			mem_store 	<= 0;
 			alu_op 		<= "0000";
-			imm 			<= 0;
-			bn 				<= 0;
-			bz 				<= 0;
-			b 				<= 0;
-			load 			<= 0;
+			imm 		<= 0;
+			bn 		<= 0;
+			bz 		<= 0;
+			b 		<= 0;
+			load 		<= 0;
 		end if;
 	end process;
 
